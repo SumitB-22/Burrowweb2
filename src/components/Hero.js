394d58d8f79357173aec4d5a297831f1fcb7a4c3
sumit <sumitbhagat3228@gmail.com>
2025-02-19 19:01:56 +0530

    images addes to ContactUs and Home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Hero.css';
 
 const Hero = () => (
   <section className="hero">
     <h1>Welcome to Burrow Space!</h1>
     <p>Your Data, Your Control—Seamlessly.</p>
-    <a href="/about">Learn More</a>
+    <Link to="/about" className="learn-more-button">Learn More</Link>
   </section>
 );
 
